e22a8118e3b22fd387a81c8878ffda78271cb206
ajdomingues <12472840+ajdomingues@users.noreply.github.com>
2024-03-06 21:47:02 -0300

    Atualização de documentação.

--- js/1035.js
+++ js/1035.js
@@ -4,5 +4,5 @@ const [A, B, C, D] = lines[0].split(' ').map(Number);
 if ((B > C) && (D > A) && ((C + D) > (A + B)) && ((C > 0) && (D > 0)) && (A % 2 == 0)) {
     console.log('Valores aceitos');
 } else {
-    console.log('Valores não aceitos');
+    console.log('Valores nao aceitos');
 }
