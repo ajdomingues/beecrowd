3d6620587feeb49fb0a24077c6dbf6ee73dfb1a5
ajdomingues <12472840+ajdomingues@users.noreply.github.com>
2024-04-20 09:43:42 -0300

    Add 1048

---
+++ js/1048.js
@@ -0,0 +1,42 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+const salario = Number(lines.shift());
+let novoSalario = 0;
+let reajusteGanho = 0;
+let percentual = 0;
+switch (true) {
+    case salario > 2000:
+        percentual = 4;
+        novoSalario = salario + (salario * (percentual / 100));
+        reajusteGanho = novoSalario - salario;
+        break;
+    case salario >= 1200.01 && salario <= 2000:
+        percentual = 7;
+        novoSalario = salario + (salario * (percentual / 100));
+        reajusteGanho = novoSalario - salario;
+        break;
+    case salario >= 800.01 && salario <= 1200:
+        percentual = 10;
+        novoSalario = salario + (salario * (percentual / 100));
+        reajusteGanho = novoSalario - salario;
+        break;
+    case salario >= 400.01 && salario <= 800:
+        percentual = 12;
+        novoSalario = salario + (salario * (percentual / 100));
+        reajusteGanho = novoSalario - salario;
+        break;
+    case salario <= 400:
+        percentual = 15;
+        novoSalario = salario + (salario * (percentual / 100));
+        reajusteGanho = novoSalario - salario;
+        break;
+}
+
+console.log(`Novo salario: ${novoSalario.toFixed(2)}
+Reajuste ganho: ${reajusteGanho.toFixed(2)}
+Em percentual: ${percentual} %`);
+
+percentual = 10;
+reajusteGanho = salario * (percentual / 100);
+novoSalario = salario + reajusteGanho;
