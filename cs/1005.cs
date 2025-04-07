dd175e8ba47b54b82d8950b957af546c0365fe42
ajdomingues <12472840+ajdomingues@users.noreply.github.com>
2025-04-07 15:29:21 -0300

    Exercício 1005 em c#.

---
+++ cs/1005.cs
@@ -0,0 +1,4 @@
+double A = double.Parse(Console.ReadLine());
+double B = double.Parse(Console.ReadLine());
+double media = (A * 3.5 + B * 7.5) / 11;
+Console.WriteLine($"MEDIA = {media.ToString("0.00000")}");
