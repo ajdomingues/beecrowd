c26129811155c68605795b709f14fbaace20eeed
ajdomingues <12472840+ajdomingues@users.noreply.github.com>
2025-04-09 21:16:20 -0300

    Exercício 1006 em c#

---
+++ cs/1006.cs
@@ -0,0 +1,5 @@
+double A = double.Parse(Console.ReadLine());
+double B = double.Parse(Console.ReadLine());
+double C = double.Parse(Console.ReadLine());
+double media = ((A * 2) + (B * 3) + (C * 5)) / 10;
+Console.WriteLine($"MEDIA = {media.ToString("0.0")}");
