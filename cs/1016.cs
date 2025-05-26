834dd7f344f6fd2684e99166c7c4b88dae89cc11
ajdomingues <12472840+ajdomingues@users.noreply.github.com>
2025-05-26 16:10:07 -0300

    1016 em c#

---
+++ cs/1016.cs
@@ -0,0 +1,4 @@
+int distanciaPercorrida = int.Parse(Console.ReadLine());
+const int mediaDistanciamento = 1;
+double tempoDistanciamento = distanciaPercorrida * (mediaDistanciamento * 2);
+Console.WriteLine($"{tempoDistanciamento} minutos");
