0c8c45e95f342ac562d4fa92e8c10857249b40aa
ajdomingues <12472840+ajdomingues@users.noreply.github.com>
2025-08-18 08:47:49 -0300

    Feat 1019.

--- cs/1019.cs
+++ cs/1019.cs
@@ -3,3 +3,19 @@ int horas = 0;
 int minutos = 0;
 int segundos = 0;
 int temporario = tempo;
+
+for (int i = 1; i <= tempo; i++)
+{
+    if (temporario >= 3600)
+    {
+        horas++;
+        temporario -= 3600;
+    }
+    if (temporario >= 60) {
+        minutos++;
+        temporario -= 60;
+    }
+
+}
+segundos = temporario;
+Console.WriteLine($"{horas}:{minutos}:{segundos}");
