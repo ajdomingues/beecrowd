b38f8cb7a07b5a819292d99ebc737a97925546a3
ajdomingues <12472840+ajdomingues@users.noreply.github.com>
2025-08-26 12:19:53 -0300

    🚀 Feat 1018 em C# v3

---
+++ cs/1018-v3.cs
@@ -0,0 +1,15 @@
+int valor = int.Parse(Console.ReadLine());
+Console.WriteLine(valor);
+Console.WriteLine($"{valor / 100} nota(s) de R$ 100,00");
+valor %= 100;
+Console.WriteLine($"{valor / 50} nota(s) de R$ 50,00");
+valor %= 50;
+Console.WriteLine($"{valor / 20} nota(s) de R$ 20,00");
+valor %= 20;
+Console.WriteLine($"{valor / 10} nota(s) de R$ 10,00");
+valor %= 10;
+Console.WriteLine($"{valor / 5} nota(s) de R$ 5,00");
+valor %= 5;
+Console.WriteLine($"{valor / 2} nota(s) de R$ 2,00");
+valor %= 2;
+Console.WriteLine($"{valor / 1} nota(s) de R$ 1,00");
